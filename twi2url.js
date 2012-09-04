c0b285f3e50b87863e4c0bdfaf40841b12f3ee37
take_cheeze <takechi101010@gmail.com>
2012-09-04 09:31:39 +0900

    fix variable name

--- twi2url.js
+++ twi2url.js
@@ -413,7 +413,7 @@ function get_json(url, callback) {
           default:
           console.error("Error fetching json from twitter:", res.statusCode);
           console.error('URL: ' + url);
-          console.error('OAuth Param; ', oauth);
+          console.error('OAuth Param; ', opt);
           break;
         }
       }
