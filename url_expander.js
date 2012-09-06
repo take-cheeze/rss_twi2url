3e1d530eae1534133539d261f4812d54cac6db53
take_cheeze <takechi101010@gmail.com>
2012-09-06 09:38:37 +0900

    use http.request directly in url expander

--- url_expander.js
+++ url_expander.js
@@ -1,8 +1,18 @@
 module.exports = function(url, cb, timeout) {
-  require('request').head(
-    { url: url, timeout: timeout, followAllRedirects: true },
-    function(err, res) {
-      var result = err? url : res.request.href;
-      cb(result);
-    });
+  var parsed_url = require('url').parse(url);
+  var req =
+    require('http').request({
+      host: parsed_url.hostname,
+      path: (parsed_url.pathname || '') + (parsed_url.search || ''),
+      method: 'HEAD'
+    }, function(res) {
+         res.on('end', function(chunk) {
+           cb((res.headers &&
+               res.headers.location &&
+               (res.headers.location.indexOf('http') > -1))
+             ? res.headers.location : url);
+         });
+       });
+  req.on('error', function(e) { cb(url); });
+  req.end();
 };
