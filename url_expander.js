ca37f209a43b49be62371cde3251789bda8a2783
take_cheeze <takechi101010@gmail.com>
2012-09-22 15:13:14 +0900

    add fallback

--- url_expander.js
+++ url_expander.js
@@ -6,7 +6,7 @@ function expand_url(url, cb, timeout) {
   request.head(
     { 'timeout': timeout, 'url': url, maxRedirects: MAX_REDIRECTS },
     function(err, res) {
-      cb(err? url : res.request.href);
+      cb((err? url : res.request.href) || url);
     });
 }
 
