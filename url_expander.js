60f5fd9950f2a1b52730ce6313c662badc7e4b3b
take_cheeze <takechi101010@gmail.com>
2012-09-22 15:47:17 +0900

    don't use global agent

--- url_expander.js
+++ url_expander.js
@@ -4,7 +4,8 @@ var MAX_REDIRECTS = 3;
 
 function expand_url(url, cb, timeout) {
   request.head(
-    { 'timeout': timeout, 'url': url, maxRedirects: MAX_REDIRECTS },
+    { 'timeout': timeout, 'url': url,
+      maxRedirects: MAX_REDIRECTS, pool: false },
     function(err, res) {
       cb((err? url : res.request.href) || url);
     });
